d5a3817ff87d34be79993bae50004451e6403e08
shella12 <ayeshaarshad4567@gmail.com>
2023-02-10 14:39:26 +0500

    fix linter errors

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
+gem 'rubocop', '>= 1.0', '< 2.0'
 source 'https://rubygems.org'
 
 # gem "rails"
--- app.rb
+++ app.rb
@@ -1,33 +1,31 @@
-$morse_code = {A:".-", B:"-...", C:"-.-.", D:"-..", E:".", F:"..-.",
-G:"--.", H:"....", I:"..", J: ".---", K:"-.-", L:".-..",
-M:"--", N:"-.", O:"---", P:".--.", Q:"--.-", R:".-.",
-S:"...", T:"-", U:"..-", V:"...-", W:".--", X:"-..-",
-Y:"-.--", Z:"--.."} 
+$morse_code = { A: '.-', B: '-...', C: '-.-.', D: '-..', E: '.', F: '..-.',
+                G: '--.', H: '....', I: '..', J: '.---', K: '-.-', L: '.-..',
+                M: '--', N: '-.', O: '---', P: '.--.', Q: '--.-', R: '.-.',
+                S: '...', T: '-', U: '..-', V: '...-', W: '.--', X: '-..-',
+                Y: '-.--', Z: '--..' }
 
 # character decoding method
 
-def decode_char (chr)
-char = ''
+def decode_char(chr)
+  char = ''
 
-$morse_code.each do 
-	|key,value| 
-	char = key.to_s if chr == value 
-end
-	char
+  $morse_code.each do |key, value|
+    char = key.to_s if chr == value
+  end
+  char
 end
 
-decode_char("-...")
+decode_char('-...')
 
 # Word decoding method
 
-def decode_word (word)
-	decoded_word = ''
-	word = word.split
-	word.each do 
-		|i|
-		decoded_word += decode_char(i)
-end
-	decoded_word
+def decode_word(word)
+  decoded_word = ''
+  word = word.split
+  word.each do |i|
+    decoded_word += decode_char(i)
+  end
+  decoded_word
 end
 
-decode_word ("-- -.--")
+decode_word('-- -.--')
