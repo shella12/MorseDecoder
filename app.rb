d9ef2778437c042d2cd38619100ac67d889e3995
MosDevx <mosesk.wamae@gmail.com>
2023-02-10 12:32:04 +0300

    Implement decode_message method

--- app.rb
+++ app.rb
@@ -9,10 +9,12 @@ Y:"-.--", Z:"--.."}
 def decode_char (chr)
 char = ''
 
-$morse_code.each do 
-	|key,value| 
-	char = key.to_s if chr == value 
-end
+# $morse_code.each do 
+# 	|key,value| 
+# 	char = key.to_s if chr == value 
+# end
+	char = $morse_code.key(chr).to_s
+
 	char
 end
 
@@ -31,3 +33,18 @@ end
 end
 
 decode_word ("-- -.--")
+
+
+# Messsage decoding method
+
+def decode_message(msg)
+	decoded_msg = ''
+	msg = msg.split('   ')
+
+	msg.each do |word|
+		decoded_msg += decode_word(word) + ' '
+	end
+	decoded_msg.strip
+end
+
+decode_message(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
